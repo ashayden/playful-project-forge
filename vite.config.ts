56c9ec5f8668813aba84d29b0a25f51b176db8fb
ashayden <ashayden@gmail.com>
2025-01-22 10:40:23 -0600

    fix: move PostCSS config into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +27,14 @@ export default defineConfig({
         secure: false,
         ws: true,
       },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
     },
   },
   build: {
